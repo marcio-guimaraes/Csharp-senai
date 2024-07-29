29e33615258154e47279963bc8da87eb624df04e
marcio-guimaraes <mv601200@gmail.com>
2024-07-29 17:40:23 -0300

    update WebApi

--- Exo.WebApi/Models/Projeto.cs
+++ Exo.WebApi/Models/Projeto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace Exo.WebApi.Models
 {
     public class Projeto
--- Exo.WebApi/Program.cs
+++ Exo.WebApi/Program.cs
@@ -1,32 +1,19 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configura o DbContext com SQL Server
-builder.Services.AddDbContext<ExoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Adiciona serviços de controllers
+builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
-
-// Configura o repositório
-builder.Services.AddTransient<ProjetoRepository>();
+builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 
 var app = builder.Build();
 
-// Configure o pipeline HTTP
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
 app.UseRouting();
-app.UseAuthorization();
 
-// Usando rotas de nível superior
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
